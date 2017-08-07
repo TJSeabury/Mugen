c5d589ee0c633dffb9d50cf99caee8e25fcf0a3a
TJSeabury <tylerseabury@gmail.com>
2017-08-07 00:09:48 -0400

    Experimenting with basic shortcodes.

--- mugen.php
+++ mugen.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: 無限 Mugen
-Plugin URI:  https://github.com/TJSeabury
+Plugin URI:  https://github.com/TJSeabury/Mugen
 Description: Assorted content modules.
 Version:     0.0.1
 Author:      Tyler Seabury
@@ -37,6 +37,61 @@ if ( defined( 'MUGEN_DEBUG' ) && constant( 'MUGEN_DEBUG' ) )
 
 add_action( 'admin_menu', 'mugen_options_page' );
 
+add_action('init', 'mugen_shortcodes_init');
+
+function mugen_box_shortcode( $atts = [], $content = null, $tag = '' )
+{
+    $atts = array_change_key_case( (array)$atts, CASE_LOWER );
+    $mugen_atts = shortcode_atts( ['title' => 'WordPress.org'], $atts, $tag );
+    $o = '<style>.mugen-box{ background-color:#f2efef; box-shadow:0 4px 4px -2px hsla(0,0%,0%,0.5); padding:2rem; }</style>';
+    $o .= '<div class="mugen-box">';
+    $o .= '<h2>' . esc_html__( $mugen_atts['title'], 'mugen' ) . '</h2>';
+    if ( !is_null( $content ) )
+    {
+        $o .= apply_filters( 'the_content', $content );
+        $o .= do_shortcode( $content );
+    }
+    $o .= '</div>';
+    return $o;
+}
+
+function mugen_button_shortcode( $atts = [], $content = null, $tag = '' )
+{
+    $atts = array_change_key_case( (array)$atts, CASE_LOWER );
+    $mugen_atts = shortcode_atts( ['href' => '#'], $atts, $tag );
+    $o = '<style>
+    .mugen-button {
+        position: relative;
+        display: inline-block;
+        color: white !important;
+        text-transform: uppercase;
+        font-weight: 100;
+        letter-spacing: 2px;
+        background-color: hsla(340,100%,50%,1);
+        box-shadow: 0 2px 4px -2px hsla(0,0%,0%,0.3), 1px 2px 4px 0 hsla(0,0%,0%,0.1), -1px 2px 4px 0 hsla(0,0%,0%,0.1) !important;
+        padding: 0.2rem 1rem;
+        transition: all 150ms ease-out !important;
+    }
+    .mugen-button:hover {
+        box-shadow: 0 4px 4px -2px hsla(0,0%,0%,0.3), 4px 4px 10px 0 hsla(0,0%,0%,0.1), -4px 4px 10px 0 hsla(0,0%,0%,0.1) !important;
+        transform: translateY(-2px);
+    }
+    </style>';
+    $o .= '<a class="mugen-button" href="' . esc_html__( $mugen_atts['href'], mugen ) . '" >';
+    if ( !is_null( $content ) )
+    {
+        $o .= $content;
+    }
+    $o .= '</a>';
+    return $o;
+}
+ 
+function mugen_shortcodes_init()
+{
+    add_shortcode( 'mugen-box', 'mugen_box_shortcode' );
+    add_shortcode( 'mugen-button', 'mugen_button_shortcode' );
+}
+
 function mugen_options_page_html()
 {
     // check user capabilities
@@ -49,11 +104,11 @@ function mugen_options_page_html()
         <h1><?= esc_html( get_admin_page_title() ); ?></h1>
         <form action="options.php" method="post">
             <?php
-            // output security fields for the registered setting "wporg_options"
-            //settings_fields('wporg_options');
+            // output security fields for the registered setting "mugen_options"
+            //settings_fields('mugen_options');
             // output setting sections and their fields
-            // (sections are registered for "wporg", each field is registered to a specific section)
-            //do_settings_sections('wporg');
+            // (sections are registered for "mugen", each field is registered to a specific section)
+            //do_settings_sections('mugen');
             // output save settings button
             //submit_button('Save Settings');
             ?>
